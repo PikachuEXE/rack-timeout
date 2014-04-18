5ff0535d75c2c267145f69606e16b09d661e721f
Kivanio Barbosa <kivanio@gmail.com>
2014-04-18 10:50:20 -0400

    use syntax for rails 4

--- lib/rack-timeout.rb
+++ lib/rack-timeout.rb
@@ -2,9 +2,15 @@
 require 'rack/timeout'
 require 'rack/timeout/logger'
 
-if defined?(Rails) && [3,4].include?(Rails::VERSION::MAJOR) && !Rails.env.test?
+if defined?(Rails) && [3, 4].include?(Rails::VERSION::MAJOR)
   class Rack::Timeout::Railtie < Rails::Railtie
-    initializer('rack-timeout.prepend') { |app| app.config.middleware.insert 0, Rack::Timeout }
+    initializer('rack-timeout.prepend') do |app|
+      if Rails::VERSION::MAJOR >= 4
+        app.middleware.use Rack::Timeout
+      else
+        app.config.middleware.insert 0, Rack::Timeout
+      end
+    end
   end
 end
 
