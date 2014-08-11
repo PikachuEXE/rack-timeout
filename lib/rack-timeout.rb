1853f81edae037a8f4585e714c72d06b5ba890ce
PikachuEXE <pikachuexe@gmail.com>
2014-08-11 15:42:36 +0800

    ! Fix the middleware is not inserted at top when initialising

--- lib/rack-timeout.rb
+++ lib/rack-timeout.rb
@@ -6,7 +6,7 @@ if defined?(Rails) && [3, 4].include?(Rails::VERSION::MAJOR)
   class Rack::Timeout::Railtie < Rails::Railtie
     initializer('rack-timeout.prepend') do |app|
       if Rails::VERSION::MAJOR >= 4
-        app.middleware.use Rack::Timeout
+        app.middleware.insert 0, Rack::Timeout
       else
         app.config.middleware.insert 0, Rack::Timeout
       end
